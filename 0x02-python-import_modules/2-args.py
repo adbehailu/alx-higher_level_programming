e8187978fb483929d9e09eeaa3d42fc35f8dec83
adbehailu <adbehailu@gmail.com>
2023-01-05 09:10:13 -0800

    make a script dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
         print("{} arguments:".format(count - 1))
     else:
         print("{} arguments:".format(count - 1))
-        for i in range(1, count):
-            print("{}: {}".format(i, sys.argv[i]))
+
+    for i in range(1, count):
+        print("{}: {}".format(i, sys.argv[i]))
